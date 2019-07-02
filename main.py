e740d96ad80b42b2f9adc96814e11e4418e17e5a
Chernyshov <xpom55@mail.ru>
2019-07-03 00:00:20 +0300

    ~ before deleting all code ...

--- main.py
+++ main.py
@@ -23,6 +23,27 @@ class MyLinkedList(object):
       :rtype: int
     """
 
+    currentNode = None
+    nextNode = None
+    if self.root != None:
+      currentNode = self.list[self.root]
+      nextNode = currentNode['next']
+    currentKey = self.root
+    count = 0
+
+    while nextNode != None and count != index:
+      count += 1
+      currentNode = self.list[nextNode]
+      nextNode = currentNode['next']
+      currentKey = nextNode
+
+    if count == index and currentNode != None:
+      # print('get found! index', index, currentNode['value'])
+      return currentNode['value']
+
+    print('get NOT found')
+    return -1
+
   def addAtHead(self, val):
     """
         Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
@@ -33,14 +54,13 @@ class MyLinkedList(object):
     if self.root == None:
       self.root = self.countKey
       self.list[self.countKey] = node
-      print(self.list)
+      # print(self.list)
       return
 
-    currentNode = self.list[self.root]
     node['next'] = self.root
     self.list[self.countKey] = node
     self.root = self.countKey
-    print('last node', self.list[self.countKey])
+    # print('last node', self.list[self.countKey])#
 
   def addAtTail(self, val):
     """
@@ -52,22 +72,22 @@ class MyLinkedList(object):
     if self.root == None:
       self.root = self.countKey
       self.list[self.countKey] = node
-      print(self.list)
+      # print(self.list)
       return
 
     currentNode = self.list[self.root]
     nextNode = currentNode['next']
     currentKey = self.root
-    print('root node', currentNode)
+    # print('root node', currentNode)
     while nextNode != None:
       currentNode = self.list[nextNode]
       nextNode = currentNode['next']
       currentKey = nextNode
-      print('find', currentNode)
+      # print('find', currentNode)
 
     currentNode['next'] = self.countKey
     self.list[self.countKey] = node
-    print('last node', self.list[self.countKey])
+    # print('last node', self.list[self.countKey])
 
   def addAtIndex(self, index, val):
     """
@@ -76,13 +96,16 @@ class MyLinkedList(object):
       :type val: int
       :rtype: None
     """
-    currentNode = self.list[self.root]
-    nextNode = currentNode['next']
-    currentKey = self.root
+    currentNode = None
+    nextNode = None
     prevNode = None
-    count = 0
+    if self.root != None:
+      currentNode = self.list[self.root]
+      nextNode = currentNode['next']
+    currentKey = self.root
+    count = 1
 
-    while nextNode != None and count != index:
+    while nextNode != None and count != index and currentKey != None:
       currentNode = self.list[nextNode]
       nextKey = currentNode['next']
       prevNode = self.list[currentKey]
@@ -91,12 +114,14 @@ class MyLinkedList(object):
       print('currentKey', currentKey)
 
     print('>>> count', count, 'index', index, 'prevNode', prevNode)
-    if prevNode == None and count == 0:  # add in root
+    if prevNode == None and index == 0:  # add in root
+      print('add  at heads')
       self.addAtHead(val)
-    elif prevNode == None and count + 1 == index:  # add in lastNode
+    elif prevNode != None and nextNode == None:  # add in lastNode
+      print('add at last')
       self.addAtTail(val)
-    elif count == index:
-      print('Found!', currentKey)
+    elif count == index and prevNode != None:
+      # print('Found!', currentKey)
       node = self.initNode(val)
       node['next'] = prevNode['next']
       prevNode['next'] = self.countKey
@@ -110,6 +135,37 @@ class MyLinkedList(object):
       :type index: int
       :rtype: None
     """
+    currentNode = None
+    nextNode = None
+    if self.root != None:
+      currentNode = self.list[self.root]
+      if currentNode['next'] != None:
+        nextNode = self.list[currentNode['next']]
+
+    currentKey = self.root
+    prevNode = None
+    count = 0
+
+    while currentNode != None and nextNode != None and count != index:
+      prevNode = currentNode
+      # print('nextNode', nextNode)
+      currentNode = nextNode
+      if currentNode['next'] != None:
+        nextNode = self.list[currentNode['next']]
+      currentKey = prevNode['next']
+      count += 1
+      # print('currentKey', currentKey)
+
+    # print('>>> count', count, 'index', index, 'prevNode', prevNode)
+    if prevNode == None and count == 0 and currentNode != None:  # add in root
+      self.root = currentNode['next']
+    elif prevNode == None and count + 1 == index:  # add in lastNode
+      prevNode['next'] = None
+    elif count == index:
+      # print('Found!', currentKey)
+      prevNode['next'] = currentNode['next']
+    else:
+      print('Not Found!', index)
 
 
 # Your MyLinkedList object will be instantiated and called as such:
@@ -125,19 +181,41 @@ linkedList = MyLinkedList()
 # linkedList.addAtIndex(0, 1)
 # linkedList.printList()
 
-linkedList.addAtHead(1)
-linkedList.addAtTail(3)
-linkedList.addAtIndex(1, 2)
-linkedList.addAtIndex(3, 4)
-linkedList.addAtTail(5)
-linkedList.addAtHead(0)
-linkedList.printList()
+# ["MyLinkedList","addAtHead","addAtTail","addAtIndex","get","deleteAtIndex","get"]
+# [[],[1],[3],[1,2],[1],[1],[1]]
+# print(linkedList.addAtHead(1))
+# print(linkedList.addAtTail(3))
+# print(linkedList.addAtIndex(1, 2))
+# print(linkedList.get(1))
+# print(linkedList.deleteAtIndex(1))
+# print(linkedList.get(1))
 
-# linkedList.addAtTail(val)
-# linkedList.addAtIndex(index,val)
-# linkedList.deleteAtIndex(index)
+# ["MyLinkedList","addAtHead","addAtTail","addAtIndex","get","deleteAtIndex","get"]
+# [[],[1],[3],[4,2],[1],[-1],[1]]
+# print(linkedList.addAtHead(1))
+# print(linkedList.addAtTail(3))
+# print(linkedList.addAtIndex(4, 2))
+# print(linkedList.get(1))
+# print(linkedList.deleteAtIndex(-1))
+# print(linkedList.get(1))
 
-# linkedList.addAtIndex(1, 2);  // linked list becomes 1->2->3
-# linkedList.get(1);            // returns 2
-# linkedList.deleteAtIndex(1);  // now the linked list is 1->3
-# linkedList.get(1);            // returns 3
+# ["MyLinkedList","addAtHead","addAtIndex","get","get","get"]
+# [[],[1],[1,2],[1],[0],[2]]
+# print(linkedList.addAtHead(1))
+# print(linkedList.addAtIndex(1, 2))
+# print(linkedList.printList())
+# print(linkedList.get(1))
+# print(linkedList.get(0))
+# print(linkedList.get(2))
+
+["MyLinkedList", "get", "addAtIndex", "get", "get", "addAtIndex", "get", "get"]
+[[], [0], [1, 2], [0], [1], [0, 1], [0], [1]]
+print('>>>', linkedList.get(0))
+print('>>>', linkedList.addAtIndex(1, 2))
+print('>>>', linkedList.get(0))
+# print(linkedList.get())
+# print(linkedList.addAtIndex())
+# print(linkedList.get())
+# print(linkedList.get())
+
+# print(linkedList.())
